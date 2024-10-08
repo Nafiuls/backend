5869c44a6dc0e067223905bcabca0e2240cf7438
Nafiuls <snafiul700@gmail.com>
2024-10-09 00:56:35 +0600

    try

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ const PORT = process.env.PORT || 5000;
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //   })
 // );
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://auth-project-770fb.web.app',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.get('/', (req, res) => {
