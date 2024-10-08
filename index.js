6eb0dd395e983eded81ae0656c6242ab4426f01b
Nafiuls <snafiul700@gmail.com>
2024-10-09 00:52:50 +0600

    cors

--- index.js
+++ index.js
@@ -17,17 +17,18 @@ const PORT = process.env.PORT || 5000;
 // };
 
 // middlewares
-app.use(
-  cors({
-    origin: [
-      'https://auth-project-770fb.web.app',
-      'http://localhost:5173',
-      'https://auth-project-770fb.firebaseapp.com',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       'https://auth-project-770fb.web.app',
+//       'http://localhost:5173',
+//       'https://auth-project-770fb.firebaseapp.com',
+//     ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.get('/', (req, res) => {
