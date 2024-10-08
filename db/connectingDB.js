e471134a26b16e7a4fb03fdc233f2c6b8176207c
Nafiuls <snafiul700@gmail.com>
2024-10-08 17:40:08 +0600

    testing done

--- db/connectingDB.js
+++ db/connectingDB.js
@@ -1,13 +1,13 @@
-// import mongoose from 'mongoose';
-// const connection = async () => {
-//   try {
-//     const conn = await mongoose.connect(
-//       'mongodb+srv://snafiul700:dB3BsPVvJdTFowgT@cluster0.uiheq.mongodb.net/auth_db?retryWrites=true&w=majority&appName=Cluster0'
-//     );
-//     console.log(`connected to ${conn.connection.host}`);
-//   } catch (error) {
-//     console.log('Error connecting to MONGODB:', error.message);
-//     process.exit(1); // which means failure 0 is successful
-//   }
-// };
-// export default connection;
+import mongoose from 'mongoose';
+const connection = async () => {
+  try {
+    const conn = await mongoose.connect(
+      'mongodb+srv://snafiul700:dB3BsPVvJdTFowgT@cluster0.uiheq.mongodb.net/auth_db?retryWrites=true&w=majority&appName=Cluster0'
+    );
+    console.log(`connected to ${conn.connection.host}`);
+  } catch (error) {
+    console.log('Error connecting to MONGODB:', error.message);
+    process.exit(1); // which means failure 0 is successful
+  }
+};
+export default connection;
