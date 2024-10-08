cc6e2b14a5c4ec5cf311e710f5f4b24d0ebad196
Nafiuls <snafiul700@gmail.com>
2024-10-08 19:44:26 +0600

    done

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const corsOptions = {
-  origin: ['https://auth-project-770fb.web.app'],
-  credentials: true, // Enable sending cookies with requests
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // List the methods you're using// Include any custom headers
-};
+// const corsOptions = {
+//   origin: ['https://auth-project-770fb.web.app'],
+//   credentials: true, // Enable sending cookies with requests
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // List the methods you're using// Include any custom headers
+// };
 
 // middlewares
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-app.use(express.json()); //this will allow us to parese the incoming request with json payload (req.body)
+app.use(
+  cors({
+    origin: ['https://auth-project-770fb.web.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
+app.use(express.json());
 app.use(cookieParser());
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
