20db45584ec8732bce8c475118978658b2d66b12
Nafiuls <snafiul700@gmail.com>
2024-10-08 19:14:40 +0600

    corss

--- index.js
+++ index.js
@@ -11,21 +11,16 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-  origin: [
-    'https://auth-project-770fb.web.app/api/auth',
-    'http://localhost:5173',
-  ],
+  origin: ['https://auth-project-770fb.web.app'],
   credentials: true, // Enable sending cookies with requests
-  // List the methods you're using// Include any custom headers
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // List the methods you're using// Include any custom headers
 };
 
 // middlewares
 app.use(cors(corsOptions));
-
 app.use(express.json()); //this will allow us to parese the incoming request with json payload (req.body)
 app.use(cookieParser());
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello from the server!');
 });
 
